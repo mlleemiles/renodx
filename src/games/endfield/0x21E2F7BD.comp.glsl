9edf0927402f6de3be93f93ae7f02caf95f30c73
mlleemiles <32784785+mlleemiles@users.noreply.github.com>
2026-02-02 22:49:45 +0900

    Add files via upload

---
+++ src/games/endfield/0x21E2F7BD.comp.glsl
@@ -0,0 +1,219 @@
+#version 450
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+layout(set = 1, binding = 0, std140) uniform type_ShaderVariablesGlobal
+{
+    layout(row_major) mat4 _ViewMatrix;
+    layout(row_major) mat4 _InvViewMatrix;
+    layout(row_major) mat4 _ProjMatrix;
+    layout(row_major) mat4 _InvProjMatrix;
+    layout(row_major) mat4 _DeprecatedViewProjMatrix;
+    layout(row_major) mat4 _ViewNoTransProjMatrix;
+    layout(row_major) mat4 _InvViewProjMatrix;
+    layout(row_major) mat4 _DeprecatedNonJitteredViewProjMatrix;
+    layout(row_major) mat4 _NonJitteredViewNoTransProjMatrix;
+    layout(row_major) mat4 _InvNonJitteredViewProjMatrix;
+    layout(row_major) mat4 _InvPretransformMatrix;
+    vec4 _WorldSpaceCameraPos_Internal;
+    layout(row_major) mat4 _DeprecatedPrevViewProjMatrix;
+    layout(row_major) mat4 _PrevViewNoTransProjMatrix;
+    layout(row_major) mat4 _DeprecatedPrevNonJitteredViewProjMatrix;
+    layout(row_major) mat4 _PrevNonJitteredViewNoTransProjMatrix;
+    layout(row_major) mat4 _PrevInvViewProjMatrix;
+    layout(row_major) mat4 _PrevNonJitteredInvViewProjMatrix;
+    layout(row_major) mat4 _ReprojectionMatrix;
+    layout(row_major) mat4 _WiderFoVViewProjMatrix;
+    layout(row_major) mat4 _WiderFoVInvViewProjMatrix;
+    vec4 _PrevCamPosRWS_Internal;
+    vec4 _ScreenSize;
+    vec4 _BackBufferSize;
+    vec4 _ZBufferParams;
+    vec4 _ProjectionParams;
+    vec4 unity_OrthoParams;
+    vec4 _ScreenParams;
+    vec4 _FrustumPlanes[6];
+    vec4 _ShadowFrustumPlanes[6];
+    vec4 _TaaFrameInfo;
+    vec4 _TaaJitterStrength;
+    vec4 _Time;
+    vec4 _SinTime;
+    vec4 _CosTime;
+    vec4 unity_DeltaTime;
+    vec4 _TimeParameters;
+    vec4 _LastTimeParameters;
+    float _GlobalMipBias;
+    float _GlobalMipBiasPow2;
+    float _ProbeExposureScale;
+    uint _FrameCount;
+    vec4 _ExposureParams;
+    ivec4 _BinningBufferOffsets;
+    vec4 _EnvironmentGlobalParams0;
+    vec4 _GraphicsFeaturesGlobalParam0;
+    vec4 _GraphicsFeaturesGlobalParam1;
+    vec4 _WindGlobalParams0;
+    vec4 _WindGlobalParams2;
+    vec4 _CharacterPositionParams0;
+    vec4 _CharacterPositionParams1;
+    vec4 _CharacterPositionParams2;
+    vec4 _CharacterPositionParams3;
+    vec4 _CharacterHeightParams;
+    vec4 _WindMotorParams0[4];
+    vec4 _WindMotorParams1[4];
+    vec4 _WindMotorParams2[4];
+    vec4 _WindMotorParams3[4];
+    vec4 _WindMotorCount;
+    vec4 _LastWindGlobalParams0;
+    vec4 _LastWindMotorParams0[4];
+    vec4 _LastWindMotorParams1[4];
+    vec4 _LastWindMotorParams3[4];
+    vec4 _FoliageInteractiveParams0;
+    vec4 _PrevFoliageInteractiveParams0;
+    vec4 _AtmosphereFogParams0;
+    vec4 _AtmosphereFogParams1;
+    vec4 _AtmosphereFogParams2;
+    vec4 _AtmosphereFogParams3;
+    vec4 _AtmosphereFogParams4;
+    vec4 _AtmosphereFogParams5;
+    vec4 _ExponentialFogParams0;
+    vec4 _ExponentialFogParams1;
+    vec4 _ExponentialFogParams2;
+    vec4 _ExponentialFogParams3;
+    vec4 _VolumetricFogParams0;
+    vec4 _VolumetricFogParams1;
+    vec4 _VolumetricFogParams2;
+    vec4 _VolumetricFogParams3;
+    vec4 _VolumetricFogParams4;
+    vec4 _HeightFogFlowNoiseParams0;
+    vec4 _HeightFogFlowNoiseParams1;
+    vec4 _FogBakeLutRescaleParams;
+    vec4 _FogBakeLutEncodeParams;
+    vec4 _FogBakeLutYawParams;
+    vec4 _CloudShadowParams0;
+    vec4 _CloudShadowParams1;
+    vec4 _CloudShadowParams2;
+    vec4 _CloudShadowParams3;
+    vec4 _Style_MatDistCoef;
+    vec4 _Style_MatFarAlb0;
+    vec4 _Style_MatFarAlb1;
+    vec4 _Style_GbFarEms;
+    vec4 _Style_GbFarDir;
+    vec4 _Style_GbCoef;
+    vec4 _VFXParams0;
+    vec4 _VFXParams1;
+    vec4 _VFXParams2;
+    vec4 _CharacterParams0;
+    vec4 _CharacterParams1;
+    vec4 _CharacterParams2;
+    vec4 _CharacterParams3;
+    vec4 _CharacterParams4;
+    vec4 _CharacterParams5;
+    vec4 _CharacterParams6;
+    vec4 _CharacterParams7;
+    vec4 _CharacterParams8;
+    vec4 _CharacterParams9;
+    vec4 _CharacterParams10;
+    vec4 _CharacterParams11;
+    vec4 _CharacterParams12;
+    vec4 _CharacterParams13;
+    vec4 _CharacterParams14;
+    vec4 _CharacterParams15;
+    vec4 _InkSimulationWorldToUV;
+    vec4 _TerrainDeformParams0;
+    vec4 _TerrainDeformParams1;
+    vec4 _TerrainClipmapParams0[2];
+    vec4 _TerrainClipmapParams1[2];
+    float _G_EnableFeatureErosionBlend;
+    float _G_EnableFeatureB;
+    float _G_EnableFeatureC;
+    float _G_EnableFeatureD;
+    vec4 _IVParam0;
+    vec4 _IVParam1;
+    vec4 _IVParam2;
+    vec4 _IVDefaultSHAr;
+    vec4 _IVDefaultSHAg;
+    vec4 _IVDefaultSHAb;
+    vec4 _IVV2Param0;
+    vec4 _IVV2Param1;
+    vec4 _IVV2Param2;
+    vec4 _IVV2Param3;
+    vec4 _WaterInteractionParams0;
+    vec4 _WaterInteractionParams1;
+    vec4 _RainWetnessGlobalParam0;
+    vec4 _RainWetnessGlobalParam1;
+    vec4 _RainWetnessGlobalParam2;
+    vec4 _RainWetnessGlobalParam3;
+    vec4 _RainWetnessGlobalParam4;
+    vec4 _RainWetnessGlobalParam5;
+    vec4 _RainWetnessGlobalParam6;
+    vec4 _RainWetnessGlobalParam7;
+    vec4 _RainWetnessGlobalParam8;
+    vec4 _RainWetnessGlobalParam9;
+    vec4 _RainWetnessGlobalParam10;
+    vec4 _VerticalOcclusionMapParam0;
+    vec4 _WaterWetnessMaskParam0;
+    vec4 _GpuClothParams;
+    vec4 _FoliageOccluderParams0;
+    vec4 _FoliageOccluderCameraPosParam;
+    vec4 _InteractRaftParams0;
+    vec4 _InteractRaftParams1;
+    layout(row_major) mat4 _FakePlanarReflectionViewProjMatrix;
+    vec4 _FakeSphericalLightSource;
+    vec4 _VolumetricComposeParams;
+    vec4 _HackTempDataBeforeCPPPlugin[32];
+} ShaderVariablesGlobal;
+
+layout(set = 0, binding = 2) uniform sampler s_linear_clamp_sampler;
+layout(set = 0, binding = 1) uniform texture2D _GTAOBlurAOTermRT;
+layout(set = 0, binding = 0, r8) uniform writeonly image2D _GTAOUpsampleAOTermRT;
+
+const float DEPTH_SIGMA = 0.1;   // depth sensitivity
+const int KERNEL_RADIUS = 1;      // 5x5 kernel
+
+void main()
+{
+    ivec2 pix = ivec2(gl_GlobalInvocationID.xy);
+    vec2 uv = (vec2(pix) + 0.5) * ShaderVariablesGlobal._ScreenSize.zw;
+
+    // Center depth
+    float centerDepth = textureLod(sampler2D(_GTAOBlurAOTermRT, s_linear_clamp_sampler), uv, 0.0).y;
+	float aoCenter = textureLod(sampler2D(_GTAOBlurAOTermRT, s_linear_clamp_sampler), uv, 0.0).x;
+
+    float aoSum = 0.0;
+    float weightSum = 0.0;
+
+    // Kernel
+    for (int y = -KERNEL_RADIUS; y <= KERNEL_RADIUS; y++)
+    {
+        for (int x = -KERNEL_RADIUS; x <= KERNEL_RADIUS; x++)
+        {
+            vec2 offset = vec2(x, y) * ShaderVariablesGlobal._ScreenSize.zw;
+            vec2 sampleUV = uv + offset;
+
+            vec2 sampleRG = textureLod(sampler2D(_GTAOBlurAOTermRT, s_linear_clamp_sampler), sampleUV, 0.0).rg;
+            float ao = sampleRG.r;
+            float depth = sampleRG.g;
+
+            // Spatial Gaussian weight
+            float spatialWeight = exp(-(x*x + y*y) / (2.0 * float(KERNEL_RADIUS*KERNEL_RADIUS)));
+
+            // Depth bilateral weight
+            float depthDiff = abs(depth - centerDepth);
+            float depthWeight = exp2(-depthDiff * 20.0);
+			
+			float aoDiff = abs(ao - aoCenter);
+
+			// AO-domain bilateral weight
+			float aoWeight = exp2(-aoDiff * 20.0);
+
+            float w = spatialWeight * depthWeight * aoWeight;
+
+            aoSum += ao * w;
+            weightSum += w;
+        }
+    }
+
+    float aoOut = aoSum / max(weightSum, 1e-5);
+
+    imageStore(_GTAOUpsampleAOTermRT, pix, vec4(aoOut, 0, 0, 0));
+}
+
---
+++ src/games/endfield/0x820102A4.comp.glsl
@@ -0,0 +1,230 @@
+// blur x
+
+#version 450
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+layout(set = 1, binding = 0, std140) uniform type_GTAOData
+{
+    vec4 _GTAOParam0;
+    vec4 _GTAOParam1;
+    vec4 _GTAOParam2;
+    vec4 _GTAOHalfScreenSize;
+} _GTAOData;
+
+layout(set = 0, binding = 4) uniform sampler s_point_clamp_sampler;
+layout(set = 0, binding = 1) uniform texture2D _GTAODepthMIPs;
+layout(set = 0, binding = 2) uniform texture2D _GTAOMainAOTermRT;
+layout(set = 0, binding = 3) uniform texture2D _GTAOBlurAOTermInRT;
+layout(set = 0, binding = 0, r8) uniform writeonly image2D _GTAOBlurAOTermOutRT;
+
+// because the dispatch size is width/8, height/8, 1
+// we don't have to worry about not fetch surrounding pixels, and do multiple passes
+
+#include "./shared.h"
+
+#define DenoiseBlurBeta shader_injection.ao_denoiser_blur_beta
+
+void XeGTAO_DecodeGatherPartial( const vec4 packedValue, out float outDecoded[4] )
+{
+
+    outDecoded[0] = packedValue.x;
+	outDecoded[1] = packedValue.y;
+	outDecoded[2] = packedValue.z;
+	outDecoded[3] = packedValue.w;
+
+}
+
+vec4 XeGTAO_UnpackEdges( float _packedVal )
+{
+    uint packedVal = uint(_packedVal * 255.5);
+    vec4 edgesLRTB;
+    edgesLRTB.x = float((packedVal >> 6) & 0x03) / 3.0;          // there's really no need for mask (as it's an 8 bit input) but I'll leave it in so it doesn't cause any trouble in the future
+    edgesLRTB.y = float((packedVal >> 4) & 0x03) / 3.0;
+    edgesLRTB.z = float((packedVal >> 2) & 0x03) / 3.0;
+    edgesLRTB.w = float((packedVal >> 0) & 0x03) / 3.0;
+
+    return clamp( edgesLRTB, 0.0, 1.0 );
+}
+
+void XeGTAO_AddSample( float ssaoValue, float edgeValue, inout float sum, inout float sumWeight )
+{
+    float weight = edgeValue;    
+
+    sum += (weight * ssaoValue);
+    sumWeight += weight;
+}
+
+void main()
+{
+	bool finalApply = false;
+	
+    const float blurAmount = (finalApply)?(DenoiseBlurBeta):(DenoiseBlurBeta/5.0);
+    const float diagWeight = 0.85 * 0.5;
+	
+    float aoTerm[2];   // pixel pixCoordBase and pixel pixCoordBase + int2( 1, 0 )
+    vec4 edgesC_LRTB[2];
+    float weightTL[2];
+    float weightTR[2];
+    float weightBL[2];
+    float weightBR[2];
+	
+    // gather edge and visibility quads, used later
+    const vec2 gatherCenter = vec2(gl_GlobalInvocationID.xy) * _GTAOData._GTAOHalfScreenSize.zw;
+	
+	vec4 edgesQ0 = textureGatherOffset(
+		sampler2D(_GTAOMainAOTermRT, s_point_clamp_sampler),
+		gatherCenter,
+		ivec2(0, 0),
+		3
+	);
+	vec4 edgesQ1 = textureGatherOffset(
+		sampler2D(_GTAOMainAOTermRT, s_point_clamp_sampler),
+		gatherCenter,
+		ivec2(2, 0),
+		3
+	);
+	vec4 edgesQ2 = textureGatherOffset(
+		sampler2D(_GTAOMainAOTermRT, s_point_clamp_sampler),
+		gatherCenter,
+		ivec2(1, 2),
+		3
+	);
+	
+	float visQ0[4];
+	XeGTAO_DecodeGatherPartial
+	(
+		textureGatherOffset(
+			sampler2D(_GTAOBlurAOTermInRT, s_point_clamp_sampler),
+			gatherCenter,
+			ivec2(0, 0),
+			0
+		),
+		visQ0
+	);
+	float visQ1[4];
+	XeGTAO_DecodeGatherPartial
+	(
+		textureGatherOffset(
+			sampler2D(_GTAOBlurAOTermInRT, s_point_clamp_sampler),
+			gatherCenter,
+			ivec2(2, 0),
+			0
+		),
+		visQ1
+	);
+	float visQ2[4];
+	XeGTAO_DecodeGatherPartial
+	(
+		textureGatherOffset(
+			sampler2D(_GTAOBlurAOTermInRT, s_point_clamp_sampler),
+			gatherCenter,
+			ivec2(0, 2),
+			0
+		),
+		visQ2
+	);
+	float visQ3[4];
+	XeGTAO_DecodeGatherPartial
+	(
+		textureGatherOffset(
+			sampler2D(_GTAOBlurAOTermInRT, s_point_clamp_sampler),
+			gatherCenter,
+			ivec2(2, 2),
+			0
+		),
+		visQ3
+	);
+	
+    for( int side = 0; side < 2; side++ )
+    {
+        const ivec2 pixCoord = ivec2( gl_GlobalInvocationID.x + side, gl_GlobalInvocationID.y );
+
+        vec4 edgesL_LRTB  = XeGTAO_UnpackEdges( (side==0)?(edgesQ0.x):(edgesQ0.y) );
+        vec4 edgesT_LRTB  = XeGTAO_UnpackEdges( (side==0)?(edgesQ0.z):(edgesQ1.w) );
+        vec4 edgesR_LRTB  = XeGTAO_UnpackEdges( (side==0)?(edgesQ1.x):(edgesQ1.y) );
+        vec4 edgesB_LRTB  = XeGTAO_UnpackEdges( (side==0)?(edgesQ2.w):(edgesQ2.z) );
+		
+		edgesC_LRTB[side]     = XeGTAO_UnpackEdges( (side==0)?(edgesQ0.y):(edgesQ1.x) );
+		
+        // Edges aren't perfectly symmetrical: edge detection algorithm does not guarantee that a left edge on the right pixel will match the right edge on the left pixel (although
+        // they will match in majority of cases). This line further enforces the symmetricity, creating a slightly sharper blur. Works real nice with TAA.
+        edgesC_LRTB[side] *= vec4( edgesL_LRTB.y, edgesR_LRTB.x, edgesT_LRTB.w, edgesB_LRTB.z );
+		
+#if 1   // this allows some small amount of AO leaking from neighbours if there are 3 or 4 edges; this reduces both spatial and temporal aliasing
+        const float leak_threshold = 2.5;
+		const float leak_strength = 0.5;
+        float edginess = (clamp(4.0 - leak_threshold - dot( edgesC_LRTB[side], vec4(1.0) ), 0.0, 1.0) / (4.0-leak_threshold)) * leak_strength;
+        edgesC_LRTB[side] = clamp( edgesC_LRTB[side] + edginess, 0.0, 1.0 );
+#endif
+
+        // for diagonals; used by first and second pass
+        weightTL[side] = diagWeight * (edgesC_LRTB[side].x * edgesL_LRTB.z + edgesC_LRTB[side].z * edgesT_LRTB.x);
+        weightTR[side] = diagWeight * (edgesC_LRTB[side].z * edgesT_LRTB.y + edgesC_LRTB[side].y * edgesR_LRTB.z);
+        weightBL[side] = diagWeight * (edgesC_LRTB[side].w * edgesB_LRTB.x + edgesC_LRTB[side].x * edgesL_LRTB.w);
+        weightBR[side] = diagWeight * (edgesC_LRTB[side].y * edgesR_LRTB.w + edgesC_LRTB[side].w * edgesB_LRTB.y);
+		
+        // first pass
+        float ssaoValue     = (side==0)?(visQ0[1]):(visQ1[0]);
+        float ssaoValueL    = (side==0)?(visQ0[0]):(visQ0[1]);
+        float ssaoValueT    = (side==0)?(visQ0[2]):(visQ1[3]);
+        float ssaoValueR    = (side==0)?(visQ1[0]):(visQ1[1]);
+        float ssaoValueB    = (side==0)?(visQ2[2]):(visQ3[3]);
+        float ssaoValueTL   = (side==0)?(visQ0[3]):(visQ0[2]);
+        float ssaoValueBR   = (side==0)?(visQ3[3]):(visQ3[2]);
+        float ssaoValueTR   = (side==0)?(visQ1[3]):(visQ1[2]);
+        float ssaoValueBL   = (side==0)?(visQ2[3]):(visQ2[2]);
+		
+        float sumWeight = blurAmount;
+        float sum = ssaoValue * sumWeight;
+		
+        XeGTAO_AddSample( ssaoValueL, edgesC_LRTB[side].x, sum, sumWeight );
+        XeGTAO_AddSample( ssaoValueR, edgesC_LRTB[side].y, sum, sumWeight );
+        XeGTAO_AddSample( ssaoValueT, edgesC_LRTB[side].z, sum, sumWeight );
+        XeGTAO_AddSample( ssaoValueB, edgesC_LRTB[side].w, sum, sumWeight );
+
+        XeGTAO_AddSample( ssaoValueTL, weightTL[side], sum, sumWeight );
+        XeGTAO_AddSample( ssaoValueTR, weightTR[side], sum, sumWeight );
+        XeGTAO_AddSample( ssaoValueBL, weightBL[side], sum, sumWeight );
+        XeGTAO_AddSample( ssaoValueBR, weightBR[side], sum, sumWeight );
+
+        aoTerm[side] = sum / sumWeight;
+		imageStore(_GTAOBlurAOTermOutRT, pixCoord, vec4(aoTerm[side]));
+	}
+
+/*
+    vec2 _49 = (vec2(gl_GlobalInvocationID.xy) + vec2(0.5)) * _GTAOData._GTAOHalfScreenSize.zw;
+    float _57 = clamp((1.0 - textureLod(sampler2D(_GTAOMainAOTermRT, s_point_clamp_sampler), _49, 0.0).z * 4.0) * 2.0, 0.0, 1.0);
+	
+    vec4 _61 = textureLod(sampler2D(_GTAODepthMIPs, s_point_clamp_sampler), _49, 0.0);
+	
+	float motionVecLength = textureLod(sampler2D(_GTAOMainAOTermRT, s_point_clamp_sampler), _49, 0.0).w;
+	
+    float _62 = _61.x;
+    float _64;
+    float _67;
+    _64 = 0.0;
+    _67 = 0.0;
+    for (float _69 = -3.0; _69 <= 3.0; )
+    {
+        vec2 _75 = ((vec2(1.0, 0.0) * _69) * _GTAOData._GTAOHalfScreenSize.zw) * _57;
+		
+		float zdiff = abs(textureLod(sampler2D(_GTAODepthMIPs, s_point_clamp_sampler), _49 + _75, 0.0).x - _62);
+		
+		float sampleZ = textureLod(sampler2D(_GTAODepthMIPs, s_point_clamp_sampler), __49 + _75, 0.0).x;
+		float zDiffFactor = pow( 1.0 / (1.0 + abs(sampleZ / _62 - 1.0)), 80.f);
+		
+		float motionVecLengthSamp = textureLod(sampler2D(_GTAOMainAOTermRT, s_point_clamp_sampler),_49 + _75, 0.0).w;
+		
+		float vDiff = abs(motionVecLength - motionVecLengthSamp);
+		float vDiffFacotr = clamp(exp(-vDiff/0.2), 0.0, 1.0);
+		
+        float _92 = zDiffFactor * vDiffFacotr;//1.0 - clamp(zdiff, 0.0, 1.0);
+        _64 += _92;
+        _67 += (textureLod(sampler2D(_GTAOBlurAOTermInRT, s_point_clamp_sampler), _49 + _75, 0.0).x * _92);
+        _69 += 1.0;
+        continue;
+    }
+    imageStore(_GTAOBlurAOTermOutRT, ivec2(gl_GlobalInvocationID.xy), vec4(_67 / max(_64, 9.9999997473787516355514526367188e-05)));
+*/
+}
+
